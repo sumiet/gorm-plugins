0d330cf1d817a70de14be535291c3a9408846567
Sumit Agarwal <sumagarwal93@gmail.com>
2020-08-15 16:17:21 +0530

    Add Model definition

--- audit/audit.go
+++ audit/audit.go
@@ -1,1 +1,16 @@
 package audit
+
+import (
+	"time"
+)
+
+// Model make Model Auditable, embed `audit.Model` into your model as anonymous field to make the model auditable
+//    type User struct {
+//      audit.Model
+//    }
+type Model struct {
+	CreatedBy string     `gorm:"column:created_by;type:varchar(150)"`
+	UpdatedBy string     `gorm:"column:updated_by;type:varchar(150)"`
+	CreatedAt *time.Time `gorm:"column:created_at;type:timestamp;default:current_timestamp"`
+	UpdatedAt *time.Time `gorm:"column:updated_at;type:timestamp;default:current_timestamp"`
+}
